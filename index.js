6eda485e68d02f45ba15afbc627be5b844a5e3c0
unknown <nandhinimadan20@gmail.com>
2020-10-22 15:59:04 -0700

    Completed index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require("express")
+const helmet=require("helmet")
+const receipe=require("./routers/Receipe")
+const instructions=require("./routers/Instructions")
+const ingredients=require("./routers/Ingredients")
+const server=express()
+const port=process.env.PORT||4000
+
+server.use(helmet())
+server.use(express.json())
+server.use(receipe)
+server.use(instructions)
+server.use(ingredients)
+
+
+//error fn
+server.use((err,req,res,next)=>{
+    console.log(err)
+    res.status(500).json({
+        message:"something went wrong",
+    })
+})
+
+server.listen(port,()=>{
+    console.log(`Running at http://localhost:${port}`)
+})
